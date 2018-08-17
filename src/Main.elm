40d82a7edb4cf2d2c97a4619701d9f5de289e2b2
Dillon Kearns <dillon@dillonkearns.com>
2018-08-17 07:20:20 -0700

    Extract Page.Home module.

--- src/Main.elm
+++ src/Main.elm
@@ -11,6 +11,7 @@ import Element.Border
 import Element.Font
 import ElmLogo
 import Html exposing (Html)
+import Page.Home
 import Style exposing (fonts, palette)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -140,20 +141,6 @@ view ({ page } as model) =
             }
 
 
-wrappedText contents =
-    Element.paragraph [] [ Element.text contents ]
-
-
-bulletPoint content =
-    "→ "
-        ++ content
-        |> wrappedText
-        |> Element.el
-            [ fonts.body
-            , Element.Font.size 25
-            ]
-
-
 mainView ({ page } as model) =
     case page of
         WhyElm ->
@@ -173,97 +160,10 @@ mainView ({ page } as model) =
                 , Element.alignTop
                 , Element.width Element.fill
                 ]
-                [ navbar model
-                , whyElmSection
-                , whyIncrementalSection
-                ]
+                (navbar model
+                    :: Page.Home.view
+                )
                 |> Element.layout []
-
-
-whyElmSection =
-    bulletSection
-        { backgroundColor = palette.highlightBackground
-        , fontColor = Element.rgb 255 255 255
-        , headingText = "Want a highly reliable & maintainable frontend?"
-        , bulletContents =
-            [ "Zero runtime exceptions"
-            , "Rely on language guarantees instead of discipline"
-            , "Predictable code - no globals or hidden side-effects"
-            ]
-        , append =
-            Element.link
-                [ Element.centerX
-                ]
-                { url = "/#why-elm"
-                , label =
-                    "Read About Why Elm?"
-                        |> wrappedText
-                        |> Element.el
-                            [ Element.Border.rounded 10
-                            , Background.color palette.light
-                            , Element.Font.color white
-                            , Element.padding 15
-                            , Element.Font.size 18
-                            , Element.pointer
-                            , Element.mouseOver
-                                [ Background.color (elementRgb 25 151 192)
-                                ]
-                            ]
-                }
-        }
-
-
-elementRgb red green blue =
-    Element.rgb (red / 255) (green / 255) (blue / 255)
-
-
-white =
-    elementRgb 255 255 255
-
-
-bulletSection { backgroundColor, fontColor, headingText, bulletContents, append } =
-    Element.column
-        [ Background.color backgroundColor
-        , Element.height (Element.shrink |> Element.minimum 300)
-        , Element.width Element.fill
-        ]
-        [ Element.column
-            [ Element.Font.color fontColor
-            , Element.centerY
-            , Element.width Element.fill
-            , Element.Font.size 55
-            , fonts.body
-            , Element.spacing 25
-            , Element.padding 30
-            ]
-            (List.concat
-                [ [ headingText
-                        |> wrappedText
-                        |> Element.el
-                            [ fonts.title
-                            , Element.centerX
-                            , Element.Font.center
-                            ]
-                  ]
-                , List.map bulletPoint bulletContents
-                , [ append ]
-                ]
-            )
-        ]
-
-
-whyIncrementalSection =
-    bulletSection
-        { backgroundColor = palette.mainBackground
-        , fontColor = palette.bold
-        , headingText = "How do I start?"
-        , bulletContents =
-            [ "One tiny step at a time!"
-            , "See how Elm fits in your environment: learn the fundamentals and ship something in less than a week!"
-            , "Elm is all about reliability. Incremental Elm Consulting gets you there reliably"
-            ]
-        , append = Element.none
-        }
 
 
 logoText =
---
+++ src/Page/Home.elm
@@ -0,0 +1,126 @@
+module Page.Home exposing (view)
+
+import Animation exposing (backgroundColor)
+import Browser
+import Browser.Dom
+import Browser.Events
+import Browser.Navigation
+import Element exposing (Element)
+import Element.Background as Background
+import Element.Border
+import Element.Font
+import ElmLogo
+import Html exposing (Html)
+import Style exposing (fonts, palette)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Task
+import Time
+import Url exposing (Url)
+import Url.Builder
+
+
+wrappedText contents =
+    Element.paragraph [] [ Element.text contents ]
+
+
+bulletPoint content =
+    "→ "
+        ++ content
+        |> wrappedText
+        |> Element.el
+            [ fonts.body
+            , Element.Font.size 25
+            ]
+
+
+view =
+    [ whyElmSection
+    , whyIncrementalSection
+    ]
+
+
+whyElmSection =
+    bulletSection
+        { backgroundColor = palette.highlightBackground
+        , fontColor = Element.rgb 255 255 255
+        , headingText = "Want a highly reliable & maintainable frontend?"
+        , bulletContents =
+            [ "Zero runtime exceptions"
+            , "Rely on language guarantees instead of discipline"
+            , "Predictable code - no globals or hidden side-effects"
+            ]
+        , append =
+            Element.link
+                [ Element.centerX
+                ]
+                { url = "/#why-elm"
+                , label =
+                    "Read About Why Elm?"
+                        |> wrappedText
+                        |> Element.el
+                            [ Element.Border.rounded 10
+                            , Background.color palette.light
+                            , Element.Font.color white
+                            , Element.padding 15
+                            , Element.Font.size 18
+                            , Element.pointer
+                            , Element.mouseOver
+                                [ Background.color (elementRgb 25 151 192)
+                                ]
+                            ]
+                }
+        }
+
+
+elementRgb red green blue =
+    Element.rgb (red / 255) (green / 255) (blue / 255)
+
+
+white =
+    elementRgb 255 255 255
+
+
+bulletSection { backgroundColor, fontColor, headingText, bulletContents, append } =
+    Element.column
+        [ Background.color backgroundColor
+        , Element.height (Element.shrink |> Element.minimum 300)
+        , Element.width Element.fill
+        ]
+        [ Element.column
+            [ Element.Font.color fontColor
+            , Element.centerY
+            , Element.width Element.fill
+            , Element.Font.size 55
+            , fonts.body
+            , Element.spacing 25
+            , Element.padding 30
+            ]
+            (List.concat
+                [ [ headingText
+                        |> wrappedText
+                        |> Element.el
+                            [ fonts.title
+                            , Element.centerX
+                            , Element.Font.center
+                            ]
+                  ]
+                , List.map bulletPoint bulletContents
+                , [ append ]
+                ]
+            )
+        ]
+
+
+whyIncrementalSection =
+    bulletSection
+        { backgroundColor = palette.mainBackground
+        , fontColor = palette.bold
+        , headingText = "How do I start?"
+        , bulletContents =
+            [ "One tiny step at a time!"
+            , "See how Elm fits in your environment: learn the fundamentals and ship something in less than a week!"
+            , "Elm is all about reliability. Incremental Elm Consulting gets you there reliably"
+            ]
+        , append = Element.none
+        }
