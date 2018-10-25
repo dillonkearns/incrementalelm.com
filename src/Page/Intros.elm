7b23fa361214a76b7826cb6bcd88b113609f3979
Dillon Kearns <dillon@dillonkearns.com>
2018-10-25 16:26:50 -0700

    Fix typo.

--- src/Page/Intros.elm
+++ src/Page/Intros.elm
@@ -44,7 +44,7 @@ view dimensions =
             , Style.fonts.body
             ]
             "We offer free intro sessions for any teams that are curious to learn more about Elm! This is a great way to gauge whether there is interest in Elm on the team, and whether it might address any relevant pain points."
-        , introInfo "Adaptable, Reliable Frontends With Elm" "Experience what it's like to make changes and do refactorings in a non-trivial Elm code base. You'll learn about some libraries that make Elm even more robust, like Elm UI, dillonkearns/elm-graphql, elm-typescript-interop, and remote-data." dimensions
+        , introInfo "Adaptable, Reliable Frontends With Elm" "Experience what it's like to make changes and do refactorings in a non-trivial Elm codebase. You'll learn about some libraries that make Elm even more robust, like Elm UI, dillonkearns/elm-graphql, elm-typescript-interop, and remote-data." dimensions
         , introInfo "How I Introduced Elm at a Fortune 10" "Learn about the conditions that made Elm the right choice for a frontend framework at a Fortune 10 company, and how we pitched it to management. You'll understand some of the reasons why the teams moved faster with fewer bugs after only a few weeks' experience with Elm. We'll wrap up with a live code demo showing how to get started introducing your first bit of Elm to a JavaScript codebase." dimensions
         ]
 
