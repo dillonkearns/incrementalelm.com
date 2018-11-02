af9149ca47a6235a67314fd21e4483ea1616d558
Dillon Kearns <dillon@dillonkearns.com>
2018-11-01 18:45:36 -0700

    Fix responsive issue.

--- src/Page/CaseStudies.elm
+++ src/Page/CaseStudies.elm
@@ -44,7 +44,7 @@ view dimensions =
         , Style.Helpers.blockQuote
             { content = "Dillon at Incremental Elm has been crucial in guiding our transition to Elm. His approach is focused on your goals, with Elm as a tool to get you there. With his coaching we got Elm up and running and shipped an entirely new product, and with exceptional speed and quality - something that has previously been a myth with highly interactive frontends. If you have the chance to work with Incremental Elm, take it."
             , author =
-                Element.row [ Element.Font.bold ]
+                Element.paragraph [ Element.Font.bold ]
                     [ Element.text "Ed Gonzalez, Co-Founder at "
                     , Style.Helpers.link { url = "https://buildrtech.com/", content = "Buildr Technologies" }
                     ]
