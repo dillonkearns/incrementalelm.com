73bd1d75251d6121e31d748fdd26c8a63360a22a
Dillon Kearns <dillon@dillonkearns.com>
2019-03-08 15:24:50 -0800

    Add test to validate markup.

---
+++ tests/TestMarkup.elm
@@ -0,0 +1,30 @@
+module TestMarkup exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Mark
+import MarkParser
+import Page
+import Page.Home
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    test "markup is valid" <|
+        \() ->
+            Page.all
+                |> List.filterMap
+                    (\page ->
+                        case page.body |> Mark.parse MarkParser.document of
+                            Err error ->
+                                String.join "\n"
+                                    [ page.title
+                                    , error |> Debug.toString
+                                    ]
+                                    |> Just
+
+                            Ok _ ->
+                                Nothing
+                    )
+                |> Expect.equal []
