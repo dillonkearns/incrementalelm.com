0a9f6521ada8dcce4f35da399a33adebc5837049
Dillon Kearns <dillon@dillonkearns.com>
2019-03-13 14:11:07 -0700

    Update copy.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -25,9 +25,16 @@ parsedMarkup dimensions =
 
 We've been down this path so you don't have to. And we have the proven training material and coding techniques to put your team on the fast track to writing code like Elm Experts.
 
+Here's a blog post that will teach you one of my techniques {Link|writing Elm quickly and without getting stuck|url = https://medium.com/@dillonkearns/moving-faster-with-tiny-steps-in-elm-2e6a269e4efc} by taking tiny steps.
+
+Learn more about how our Elm Developer Support Packages can save your team time and help you deliver on Elm's promise of insanely reliable, easy to maintain applications.
+
+Or check out my free Incremental Elm Tips to learn the tricks that Elm Masters use intuitively to speed through building up Elm code with ease.
+
+Learn more about the services I provide to help your team write Elm faster and more reliably.
+
 Give your team lead a break from researching "the best way to do X in Elm", and preparing learning sessions on the basics for the rest of the team. That's what we're here for! We can get your team "thinking in Elm" with our tested teaching techniques and expert guidance.
 
-Learn more about how our Elm Developer Support Packages can save your team time and help you deliver on Elm's promise of insanely reliable, easy to maintain applications. Or check out the Incremental Elm Handbook for a condensed version of the techniques that Elm Masters use intuitively to speed through building up Elm code with ease (the first chapter is free!).
 
 """
         |> parseMarkup
