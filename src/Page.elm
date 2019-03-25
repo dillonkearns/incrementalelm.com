b2451c72064de3f7ad694e814dba219d43dfab74
Dillon Kearns <dillon@dillonkearns.com>
2019-03-25 07:44:19 -0700

    Add accelerator page.

--- src/Page.elm
+++ src/Page.elm
@@ -62,6 +62,27 @@ Learn more about how my {Link|Elm Developer Support Packages | url = /services#d
 
 """
       }
+    , { title = "Elm Accelerator Group Coaching Program"
+      , url = "accelerator-program"
+      , body = """| Header
+    Incremental Elm Accelerator Program
+
+This program is designed to help you develop the techniques and habits that get you writing effective elm code without getting stuck. You can only learn so much by reading. You need the right mix of techniques and principles, expert feedback and guidance, and seeing how experts actually write code.
+
+Breaking down the actual steps that elm experts take naturally when they're coding is key to how this program helps you succeed. The videos, exercises, and coaching in this program are designed to demysitify the techniques that elm experts use, and guide you through building up those habits to get you writing elm like a pro!
+
+| Subheader
+    Format
+
+| List
+    - 2 group coaching sessions per month
+    - Up to 5 developers per group
+    - Exclusive monthly videos
+    - Exclusive exercises
+    - Private Slack access to share progress and get help
+    - You pay for 3 months at a time. $250 x 3 = $750 per quarter
+"""
+      }
     , { title = "Incremental Elm Services"
       , url = "services"
       , body = """| Header
