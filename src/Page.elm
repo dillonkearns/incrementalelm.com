257aaf22e8299a61f604e61777d6090e1863a156
Dillon Kearns <dillon@dillonkearns.com>
2019-04-08 19:14:58 -0700

    Update copy.

--- src/Page.elm
+++ src/Page.elm
@@ -193,9 +193,11 @@ Here's an example illustrating this with {Code|USD}.
 | Header
     The Custom Scalar Checklist
 
-*Step-by-step checklist to identify the fields in your schema that should be Custom Scalars!*
+✅ Step-by-step checklist to identify the fields in your schema that should be Custom Scalars!
 
-✅ Download your free checklist!
+| Signup
+    Download the checklist
+
 
 The *Custom Scalar Checklist* goes through all the steps for identifying where you should be using Custom Scalars in your schema.
 
