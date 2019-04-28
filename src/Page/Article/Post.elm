e2441d986438e85e856f55cb64bba22fd5fc6ed1
Dillon Kearns <dillon@dillonkearns.com>
2019-04-28 10:50:06 -0500

    Update article.

--- src/Page/Article/Post.elm
+++ src/Page/Article/Post.elm
@@ -169,14 +169,14 @@ You can sign up here for more tips on writing Elm code incrementally. When you s
 Let me know how this technique goes! I’ve gotten a lot of great feedback from my clients about this approach, and I love hearing success stories. Hit reply and let me know how it goes! I’d love to hear how you’re able to apply this in your day-to-day work!
 """
       }
-    , { title = "How elm types can prevent logging users' social security #'s"
-      , pageName = "exit-checks"
+    , { title = "Using elm types to prevent logging social security #'s"
+      , pageName = "exit-gatekeepers"
       , body = """| Image
     src = /assets/article-cover/exit.jpg
-    description = Exit Checks
+    description = Exit Gatekeepers
 
 
-      One of the most successful techniques I've seen for making sure you don't break elm code the next time you touch it is a technique I call an Exit Check.
+One of the most successful techniques I've seen for making sure you don't break elm code the next time you touch it is a technique I call an *Exit Gatekeeper*.
 
 Let's say you have these innocent functions in your app. How do you know that you won't get your wires crossed and log a user's social security number?
 
@@ -215,10 +215,10 @@ The {Code|SSN} type wrapper is a good start. But how do you know it won't be unw
 
 Whoops, somebody forgot that we had a special {Code|securelySaveSSN} function that encrypts the SSN and masks the SSN when reporting errors. Do you dare look at the commit history? It could well have been your past self (we've all been there)!
 
-Humans make mistakes, so let's not expect them to be perfect. The core issue here is that the {Code|SSN} type wrapper has failed to communicate the limits of how we want it to be used. It's merely a convention to use {Code|securelySaveSSN} instead of calling the generic {Code|genericSendData} with the raw String. In this article, you'll learn a technique that gets the elm compiler to help guide us towards using data as intended: Exit Checks.
+Humans make mistakes, so let's not expect them to be perfect. The core issue here is that the {Code|SSN} type wrapper has failed to communicate the limits of how we want it to be used. It's merely a convention to use {Code|securelySaveSSN} instead of calling the generic {Code|genericSendData} with the raw String. In this article, you'll learn a technique that gets the elm compiler to help guide us towards using data as intended: Exit Gatekeepers.
 
 | Subheader
-    🔑 Exit Checks
+    🔑 Exit Gatekeepers
 
 So how do we make sure we don't log, Tweet, or otherwise misuse the user's SSN? We control the exits.
 
@@ -252,13 +252,13 @@ The public accessor function has the same effect as our publicly exposed constru
       genericSendData (ssnPayload (SSN.toString ssn)) saveSsnEndpoint
 
 | Subheader
-    Exit Checks are just gatekeepers
+    The role of a gatekeeper
 
-Think of an Exit Check like the Model in Model-View-Controller frameworks. The Model acts as a gatekeeper that ensures the integrity of all persistence in our app. Similarly, an Exit Check ensures the integrity of a Domain concept (SSNs in this case) throughout our app.
+Think of a Gatekeeper like the Model in Model-View-Controller frameworks. The Model acts as a gatekeeper that ensures the integrity of all persistence in our app. Similarly, our Exit Gatekeeper ensures the integrity of a Domain concept (SSNs in this case) throughout our app.
 
 | Subheader
     How to control the exits
-To add an Exit Check, all we need to do is define every function needed to use SSNs internally within the {Code|SSN} module. And of course, each of those functions is responsible for using it appropriately. (And on the other side of that coin, that means that the calling code is free of that responsibility!).
+To add an Exit Gatekeeper, all we need to do is define every function needed to use SSNs internally within the {Code|SSN} module. And of course, each of those functions is responsible for using it appropriately. (And on the other side of that coin, that means that the calling code is free of that responsibility!).
 
 Let's make a function to securely send an SSN. We need to guarantee that:
 | List
@@ -286,7 +286,7 @@ Now we can be confident that the calling code will never mistakenly send SSNs to
     Displaying the SSN
 What if you only want to display the last 4 digits of the SSN? How do you make sure that you, your team members, and your future self all remember to do that?
 
-You could vigilantly put that in a code review checklist, or come up with all sorts of creative heuristics to avoid that mistake. I like to reach for the Exit Check pattern as my first choice. Then you need to check very carefully any time you are modifying the SSN module itself, and you can trust the module and treat it as a blackbox when you're not modifying it.
+You could vigilantly put that in a code review checklist, or come up with all sorts of creative heuristics to avoid that mistake. I like to reach for the Exit Gatekeeper pattern as my first choice. Then you need to check very carefully any time you are modifying the SSN module itself, and you can trust the module and treat it as a blackbox when you're not modifying it.
 
 It's very likely that you'll miss something if you have to think about where SSNs are used throughout your codebase. But it's quite manageable to keep the entire SSN module in your head and feel confident that you're not forgetting anything important.
 
@@ -313,7 +313,7 @@ Here's a simple implementation of our last 4 digits view:
 
 | Subheader
     Takeaways
-You can start applying the Exit Check pattern to your elm code right away!
+You can start applying the Exit Gatekeeper pattern to your elm code right away!
 
 Here are some steps you can apply:
 
@@ -323,7 +323,7 @@ Here are some steps you can apply:
     #. Expose the constructor at first to make the change small and manageable
     #. Get everything compiling and committed!
     #. One by one, copy each function that is consuming your new Custom Type and call it from the new module
-    #. Once that's done, you can now hide the constructor, and you now have a proper Exit Check for your type!
+    #. Once that's done, you can now hide the constructor, and you now have a proper Exit Gatekeeper for your type!
 """
       }
     ]
