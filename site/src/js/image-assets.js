0f5f700cdade08178fafcb89dd2d62bae0190527
Dillon Kearns <dillon@dillonkearns.com>
2019-07-24 15:36:51 -0700

    Regenerate.

--- site/src/js/image-assets.js
+++ site/src/js/image-assets.js
@@ -1,6 +1,17 @@
 export const imageAssets = {
-  "dillon2.jpg": require("../../images/dillon2.jpg"),
+  "architecture.jpg": require("../../images/architecture.jpg"),
   "article-cover/exit.jpg": require("../../images/article-cover/exit.jpg"),
   "article-cover/mountains.jpg": require("../../images/article-cover/mountains.jpg"),
-  "article-cover/thinker.jpg": require("../../images/article-cover/thinker.jpg")
+  "article-cover/thinker.jpg": require("../../images/article-cover/thinker.jpg"),
+  "buildr-cropped.jpg": require("../../images/buildr-cropped.jpg"),
+  "contact.jpg": require("../../images/contact.jpg"),
+  "custom-scalar-checklist.pdf": require("../../images/custom-scalar-checklist.pdf"),
+  "dillon.jpg": require("../../images/dillon.jpg"),
+  "dillon2.jpg": require("../../images/dillon2.jpg"),
+  "ed-gonzalez.png": require("../../images/ed-gonzalez.png"),
+  "elm-graphql-workshop-header.jpg": require("../../images/elm-graphql-workshop-header.jpg"),
+  "graphql-workshop.png": require("../../images/graphql-workshop.png"),
+  "oslo-workshop1.jpg": require("../../images/oslo-workshop1.jpg"),
+  "steps.jpg": require("../../images/steps.jpg"),
+  "workspace.jpg": require("../../images/workspace.jpg")
 };
