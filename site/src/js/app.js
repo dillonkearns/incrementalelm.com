26d3204ec0fc0f949f649250f0c1f367001fbe12
Dillon Kearns <dillon@dillonkearns.com>
2019-07-24 15:16:00 -0700

    Import image assets from file.

--- site/src/js/app.js
+++ site/src/js/app.js
@@ -1,11 +1,5 @@
 import { Elm } from "../Main.elm";
-
-const imageAssets = {
-  "dillon2.jpg": require("../../images/dillon2.jpg"),
-  "article-cover/exit.jpg": require("../../images/article-cover/exit.jpg"),
-  "article-cover/mountains.jpg": require("../../images/article-cover/mountains.jpg"),
-  "article-cover/thinker.jpg": require("../../images/article-cover/thinker.jpg")
-};
+import { imageAssets } from "./image-assets";
 
 document.addEventListener("DOMContentLoaded", function() {
   Elm.Main.init({
