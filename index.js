e672a268900f8ca3ae99adc7eaa06936c0e8c4c7
Dillon Kearns <dillon@dillonkearns.com>
2020-02-28 06:53:04 -0800

    Add shim for custom elements.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "./lib/codemirror.css";
 import "./src/js/code-editor.js";
+import "./lib/native-shim.js";
 import "./style.css";
 const { Elm } = require("./src/Main.elm");
 const pagesInit = require("elm-pages");
