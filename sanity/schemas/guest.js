da36a509af9372c5a507bda6102223eca5d43e79
Dillon Kearns <dillon@dillonkearns.com>
2020-09-04 21:56:03 -0700

    Add guest avatar images.

--- sanity/schemas/guest.js
+++ sanity/schemas/guest.js
@@ -1,25 +1,29 @@
 export default {
-    name: 'guest',
-    type: 'document',
-    title: 'Guest',
-    fields: [
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Name',
-      },
-      {
-        name: 'twitter',
-        type: 'string',
-        title: 'Twitter',
-        description: 'Just the username, no @ or URL (e.g. dillontkearns).',
-      },
-      {
-        name: 'github',
-        type: 'string',
-        title: 'Github',
-        description: 'Just the username, no @ or URL (e.g. dillonkearns).',
-      },
-    ],
-  };
-  
+  name: 'guest',
+  type: 'document',
+  title: 'Guest',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'avatarUrl',
+      type: 'image',
+      title: 'Avatar URL',
+    },
+    {
+      name: 'twitter',
+      type: 'string',
+      title: 'Twitter',
+      description: 'Just the username, no @ or URL (e.g. dillontkearns).',
+    },
+    {
+      name: 'github',
+      type: 'string',
+      title: 'Github',
+      description: 'Just the username, no @ or URL (e.g. dillonkearns).',
+    },
+  ],
+};
