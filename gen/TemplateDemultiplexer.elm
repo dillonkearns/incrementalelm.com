e12cd44c5ddc964a0ef3d304c89296a4cbadea85
Dillon Kearns <dillon@dillonkearns.com>
2020-09-27 16:41:32 -0700

    Set up wiring of prototype code.

---
+++ gen/TemplateDemultiplexer.elm
@@ -0,0 +1,171 @@
+module TemplateDemultiplexer exposing (..)
+
+import Browser
+import Pages.Manifest as Manifest
+import Shared
+import TemplateType as M exposing (TemplateType)
+import Head
+import Html exposing (Html)
+import Pages
+import Pages.PagePath exposing (PagePath)
+import Pages.Platform
+import Pages.StaticHttp as StaticHttp
+
+
+
+type alias Model =
+    { global : Shared.Model
+    , page : TemplateModel
+    , current :
+        Maybe
+            { path :
+                { path : PagePath Pages.PathKey
+                , query : Maybe String
+                , fragment : Maybe String
+                }
+            , metadata : TemplateType
+            }
+    }
+
+
+type TemplateModel
+    = 
+    | NotFound
+
+
+
+type Msg
+    = MsgGlobal Shared.Msg
+    | OnPageChange
+        { path : PagePath Pages.PathKey
+        , query : Maybe String
+        , fragment : Maybe String
+        , metadata : TemplateType
+        }
+    | 
+
+
+view :
+    List ( PagePath Pages.PathKey, TemplateType )
+    ->
+        { path : PagePath Pages.PathKey
+        , frontmatter : TemplateType
+        }
+    ->
+        StaticHttp.Request
+            { view : Model -> Shared.RenderedBody -> { title : String, body : Html Msg }
+            , head : List (Head.Tag Pages.PathKey)
+            }
+view siteMetadata page =
+    case page.frontmatter of
+        
+
+
+init :
+    Maybe Shared.Model
+    ->
+        Maybe
+            { path :
+                { path : PagePath Pages.PathKey
+                , query : Maybe String
+                , fragment : Maybe String
+                }
+            , metadata : TemplateType
+            }
+    -> ( Model, Cmd Msg )
+init currentGlobalModel maybePagePath =
+    let
+        ( sharedModel, globalCmd ) =
+            currentGlobalModel |> Maybe.map (\m -> ( m, Cmd.none )) |> Maybe.withDefault (Shared.init maybePagePath)
+
+        ( templateModel, templateCmd ) =
+            case maybePagePath |> Maybe.map .metadata of
+                Nothing ->
+                    ( NotFound, Cmd.none )
+
+                Just meta ->
+                    case meta of
+                        
+    in
+    ( { global = sharedModel
+      , page = templateModel
+      , current = maybePagePath
+      }
+    , Cmd.batch
+        [ templateCmd
+        , globalCmd |> Cmd.map MsgGlobal
+        ]
+    )
+
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MsgGlobal msg_ ->
+            let
+                ( sharedModel, globalCmd ) =
+                    Shared.update msg_ model.global
+            in
+            ( { model | global = sharedModel }
+            , globalCmd |> Cmd.map MsgGlobal
+            )
+
+        OnPageChange record ->
+            init (Just model.global) <|
+                Just
+                    { path =
+                        { path = record.path
+                        , query = record.query
+                        , fragment = record.fragment
+                        }
+                    , metadata = record.metadata
+                    }
+
+        
+
+
+type alias SiteConfig =
+    { canonicalUrl : String
+    , manifest : Manifest.Config Pages.PathKey
+    }
+
+templateSubscriptions : TemplateType -> PagePath Pages.PathKey -> Model -> Sub Msg
+templateSubscriptions metadata path model =
+    case model.page of
+        
+
+
+        NotFound ->
+            Sub.none
+
+
+mainTemplate { documents, subscriptions, site } =
+    Pages.Platform.init
+        { init = init Nothing
+        , view = view
+        , update = update
+        , subscriptions =
+            \metadata path model ->
+                Sub.batch
+                    [ subscriptions
+                    , templateSubscriptions metadata path model
+                    ]
+        , documents = documents
+        , onPageChange = Just OnPageChange
+        , manifest = site.manifest
+        , canonicalSiteUrl = site.canonicalUrl
+        , internals = Pages.internals
+        }
+
+
+
+mapDocument : Browser.Document Never -> Browser.Document mapped
+mapDocument document =
+    { title = document.title
+    , body = document.body |> List.map (Html.map never)
+    }
+
+
+mapBoth fnA fnB ( a, b, c ) =
+    ( fnA a, fnB b, c )
