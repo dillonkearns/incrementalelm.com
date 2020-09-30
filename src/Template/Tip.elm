9731f09968c6b6a41865340060e717037faca1e6
Dillon Kearns <dillon@dillonkearns.com>
2020-09-30 09:52:54 -0700

    Add structured data to published tips.

--- src/Template/Tip.elm
+++ src/Template/Tip.elm
@@ -1,16 +1,20 @@
 module Template.Tip exposing (..)
 
+import Date exposing (Date)
 import Element
 import Element.Font as Font
 import Head
 import Head.Seo
 import Pages
-import Pages.PagePath
+import Pages.ImagePath as ImagePath
+import Pages.PagePath as PagePath exposing (PagePath)
 import Palette
 import Shared
 import Site
+import StructuredDataHelper
 import Template exposing (StaticPayload)
 import TemplateType
+import Time
 import Widget.Signup
 
 
@@ -24,15 +28,12 @@ type alias Msg =
 
 template : Template.Template_ TemplateType.TipMetadata ()
 template =
-    Template.noStaticData
-        { head =
-            \_ -> []
-        }
+    Template.noStaticData { head = head }
         |> Template.buildNoState { view = view }
 
 
 view :
-    List ( Pages.PagePath.PagePath Pages.PathKey, TemplateType.TemplateType )
+    List ( PagePath Pages.PathKey, TemplateType.TemplateType )
     -> Template.StaticPayload TemplateType.TipMetadata templateStaticData
     -> Shared.RenderedBody
     -> Shared.PageView Never
@@ -58,24 +59,64 @@ view allMetadata static viewForPage =
 head :
     StaticPayload TemplateType.TipMetadata ()
     -> List (Head.Tag Pages.PathKey)
-head { metadata } =
-    Head.Seo.summaryLarge
-        { canonicalUrlOverride = Nothing
-        , siteName = Site.name
-        , image =
-            { url = Pages.images.articleCover.lofotenHike
-            , alt = metadata.description
-            , dimensions = Nothing
-            , mimeType = Nothing
-            }
-        , description = metadata.description
-        , title = metadata.title
-        , locale = Nothing
-        }
-        |> Head.Seo.article
-            { tags = []
-            , section = Nothing
-            , publishedTime = Nothing
-            , modifiedTime = Nothing
-            , expirationTime = Nothing
-            }
+head { metadata, path } =
+    whenPublished metadata.publishedAt
+        [ Head.structuredData
+            (StructuredDataHelper.article
+                { title = metadata.title
+                , description = metadata.description
+                , author = StructuredDataHelper.person { name = "Dillon Kearns" }
+                , publisher = StructuredDataHelper.person { name = "Dillon Kearns" }
+                , url = Site.canonicalUrl ++ "/" ++ PagePath.toString path
+                , imageUrl = Site.canonicalUrl ++ "/" ++ ImagePath.toString Pages.images.articleCover.lofotenHike
+                , datePublished = Date.toIsoString metadata.publishedAt
+                , mainEntityOfPage =
+                    StructuredDataHelper.softwareSourceCode
+                        { codeRepositoryUrl = "https://github.com/dillonkearns/elm-pages"
+                        , description = "A statically typed site generator for Elm."
+                        , author = "Dillon Kearns"
+                        , programmingLanguage = StructuredDataHelper.elmLang
+                        }
+                }
+            )
+        ]
+        ++ (Head.Seo.summaryLarge
+                { canonicalUrlOverride = Nothing
+                , siteName = Site.name
+                , image =
+                    { url = Pages.images.articleCover.lofotenHike
+                    , alt = metadata.description
+                    , dimensions = Nothing
+                    , mimeType = Nothing
+                    }
+                , description = metadata.description
+                , title = metadata.title
+                , locale = Nothing
+                }
+                |> Head.Seo.article
+                    { tags = []
+                    , section = Nothing
+                    , publishedTime = Nothing
+                    , modifiedTime = Nothing
+                    , expirationTime = Nothing
+                    }
+           )
+
+
+whenPublished : Date -> List b -> List b
+whenPublished publishAt whenTrueValue =
+    if onOrAfterPublishDate Pages.builtAt publishAt then
+        whenTrueValue
+
+    else
+        []
+
+
+onOrAfterPublishDate : Time.Posix -> Date -> Bool
+onOrAfterPublishDate now publishDate =
+    let
+        zone =
+            Time.utc
+    in
+    -- now >= publishDate
+    Date.compare (Date.fromPosix zone now) publishDate /= LT
