e0c044bb4f6fd357e3c5eea0f5456f98cbe99d78
Dillon Kearns <dillon@dillonkearns.com>
2020-09-30 10:03:59 -0700

    Add Learn template.

--- src/Template/Learn.elm
+++ src/Template/Learn.elm
@@ -1,7 +1,15 @@
 module Template.Learn exposing (..)
 
 import Element
-import Template
+import Element.Font as Font
+import Head
+import Head.Seo
+import Pages
+import Pages.PagePath exposing (PagePath)
+import Shared
+import Site
+import Template exposing (StaticPayload)
+import TemplateType exposing (TemplateType)
 
 
 type alias Model =
@@ -12,14 +20,62 @@ type alias Msg =
     Never
 
 
-template : Template.Template_ templateMetadata ()
+template : Template.Template_ TemplateType.LearnMetadata ()
 template =
     Template.noStaticData
         { head =
             \_ -> []
         }
-        |> Template.buildNoState
-            { view =
-                \_ _ _ ->
-                    { title = "", body = [ Element.none ] }
+        |> Template.buildNoState { view = view }
+
+
+view :
+    List ( PagePath Pages.PathKey, TemplateType )
+    -> Template.StaticPayload TemplateType.LearnMetadata templateStaticData
+    -> Shared.RenderedBody
+    -> Shared.PageView Never
+view allMetadata static viewForPage =
+    { title = static.metadata.title
+    , body =
+        [ Element.paragraph
+            [ Font.size 36
+            , Font.center
+            , Font.family [ Font.typeface "Raleway" ]
+            , Font.bold
+            ]
+            [ Element.text static.metadata.title ]
+        , viewForPage
+            |> Element.textColumn
+                [ Element.centerX
+                , Element.width Element.fill
+                , Element.spacing 30
+                , Font.size 18
+                ]
+        ]
+    }
+
+
+head :
+    StaticPayload TemplateType.TipMetadata ()
+    -> List (Head.Tag Pages.PathKey)
+head { metadata, path } =
+    Head.Seo.summaryLarge
+        { canonicalUrlOverride = Nothing
+        , siteName = Site.name
+        , image =
+            { url = Pages.images.articleCover.lofotenHike
+            , alt = metadata.description
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = metadata.description
+        , title = metadata.title
+        , locale = Nothing
+        }
+        |> Head.Seo.article
+            { tags = []
+            , section = Nothing
+            , publishedTime = Nothing
+            , modifiedTime = Nothing
+            , expirationTime = Nothing
             }
