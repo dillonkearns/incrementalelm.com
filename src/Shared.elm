8fe3f16bfe4b2e64f314cb6c330e1701a36669d0
Dillon Kearns <dillon@dillonkearns.com>
2021-05-25 12:40:54 -0700

    Wire up Shared module.

--- src/Shared.elm
+++ src/Shared.elm
@@ -1,13 +1,28 @@
 module Shared exposing (Data, Model, Msg(..), SharedMsg(..), template)
 
+import Animation
+import Browser.Dom as Dom
+import Browser.Events
 import Browser.Navigation
 import DataSource
+import Dimensions exposing (Dimensions)
+import Ease
+import Element exposing (Element)
+import ElmLogo
 import Html exposing (Html)
+import Http
 import Pages.Flags
 import Pages.PageUrl exposing (PageUrl)
 import Path exposing (Path)
 import SharedTemplate exposing (SharedTemplate)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Task
+import Time
+import TwitchButton
 import View exposing (View)
+import View.MenuBar
+import View.Navbar
 
 
 template : SharedTemplate Msg Model Data SharedMsg msg
@@ -22,15 +37,6 @@ template =
     }
 
 
-type Msg
-    = OnPageChange
-        { path : Path
-        , query : Maybe String
-        , fragment : Maybe String
-        }
-    | SharedMsg SharedMsg
-
-
 type alias Data =
     ()
 
@@ -40,7 +46,13 @@ type SharedMsg
 
 
 type alias Model =
-    { showMobileMenu : Bool
+    { menuBarAnimation : View.MenuBar.Model
+    , menuAnimation : Animation.State
+    , dimensions : Dimensions
+    , styles : List Animation.State
+    , showMenu : Bool
+    , isOnAir : TwitchButton.IsOnAir
+    , now : Maybe Time.Posix
     }
 
 
@@ -59,24 +71,75 @@ init :
             }
     -> ( Model, Cmd Msg )
 init navigationKey flags maybePagePath =
-    ( { showMobileMenu = False }
-    , Cmd.none
+    ( { styles = ElmLogo.polygons |> List.map Animation.style
+      , menuBarAnimation = View.MenuBar.init
+      , menuAnimation =
+            Animation.style
+                [ Animation.opacity 0
+                ]
+      , dimensions =
+            Dimensions.init
+                { width = 0
+                , height = 0
+                , device = Element.classifyDevice { height = 0, width = 0 }
+                }
+      , showMenu = False
+      , isOnAir = TwitchButton.notOnAir
+      , now = Nothing
+      }
+        |> updateStyles
+    , Cmd.batch
+        [ Dom.getViewport
+            |> Task.perform InitialViewport
+        , TwitchButton.request |> Cmd.map OnAirUpdated
+        , Time.now |> Task.perform GotCurrentTime
+        ]
     )
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        OnPageChange _ ->
-            ( { model | showMobileMenu = False }, Cmd.none )
+makeTranslated i polygon =
+    polygon
+        |> Animation.interrupt
+            [ Animation.set
+                [ translate -1000
+                , Animation.scale 1
+                ]
+            , Animation.wait
+                (second
+                    * toFloat i
+                    * 0.1
+                    + (((toFloat i * toFloat i) * second * 0.05) / (toFloat i + 1))
+                    |> round
+                    |> Time.millisToPosix
+                )
+            , Animation.to
+                [ translate 0
+                , Animation.scale 1
+                ]
+            ]
 
-        SharedMsg globalMsg ->
-            ( model, Cmd.none )
+
+updateStyles : Model -> Model
+updateStyles model =
+    { model
+        | styles =
+            model.styles
+                |> List.indexedMap makeTranslated
+    }
 
 
-subscriptions : Path -> Model -> Sub Msg
-subscriptions _ _ =
-    Sub.none
+
+--update : Msg -> Model -> ( Model, Cmd Msg )
+--update msg model =
+--    case msg of
+--        OnPageChange _ ->
+--            ( { model | showMobileMenu = False }, Cmd.none )
+--
+--        SharedMsg globalMsg ->
+--            ( model, Cmd.none )
+--subscriptions : Path -> Model -> Sub Msg
+--subscriptions _ _ =
+--    Sub.none
 
 
 data : DataSource.DataSource Data
@@ -95,6 +158,196 @@ view :
     -> View msg
     -> { body : Html msg, title : String }
 view sharedData page model toMsg pageView =
-    { body = Html.div [] pageView.body
-    , title = pageView.title
+    { title = pageView.title
+    , body =
+        (if model.showMenu then
+            Element.column
+                [ Element.height Element.fill
+                , Element.alignTop
+                , Element.width Element.fill
+                ]
+                [ View.Navbar.view model animationView (toMsg StartAnimation)
+                , View.Navbar.modalMenuView model.menuAnimation
+                ]
+
+         else
+            Element.column [ Element.width Element.fill ]
+                [ View.Navbar.view model animationView (toMsg StartAnimation)
+                , pageView.body
+                    |> Element.textColumn
+                        [ Element.height Element.fill
+                        , Element.padding 30
+                        , Element.spacing 30
+                        , Element.centerX
+                        , if Dimensions.isMobile model.dimensions then
+                            Element.width (Element.fill |> Element.maximum 600)
+
+                          else
+                            Element.width (Element.fill |> Element.maximum 700)
+                        ]
+                ]
+        )
+            |> Element.layout [ Element.width Element.fill ]
     }
+
+
+animationView model =
+    svg
+        [ version "1.1"
+        , x "0"
+        , y "0"
+        , viewBox "0 0 323.141 322.95"
+        , width "100%"
+        ]
+        [ Svg.g []
+            (List.map (\poly -> polygon (Animation.render poly) []) model.styles)
+        ]
+        |> Element.html
+        |> Element.el
+            [ Element.padding 20
+            , Element.height Element.shrink
+            , Element.alignTop
+            , Element.alignLeft
+            , Element.width (Element.px 100)
+            ]
+
+
+translate n =
+    Animation.translate (Animation.px n) (Animation.px n)
+
+
+second =
+    1000
+
+
+type Msg
+    = StartAnimation
+    | Animate Animation.Msg
+    | InitialViewport Dom.Viewport
+    | WindowResized Int Int
+    | OnAirUpdated (Result Http.Error TwitchButton.IsOnAir)
+    | GotCurrentTime Time.Posix
+    | OnPageChange
+        { path : Path
+        , query : Maybe String
+        , fragment : Maybe String
+        }
+    | SharedMsg SharedMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InitialViewport { viewport } ->
+            ( { model
+                | dimensions =
+                    Dimensions.init
+                        { width = viewport.width
+                        , height = viewport.height
+                        , device =
+                            Element.classifyDevice
+                                { height = round viewport.height
+                                , width = round viewport.width
+                                }
+                        }
+              }
+            , Cmd.none
+            )
+
+        WindowResized width height ->
+            ( { model
+                | dimensions =
+                    Dimensions.init
+                        { width = toFloat width
+                        , height = toFloat height
+                        , device = Element.classifyDevice { height = height, width = width }
+                        }
+              }
+            , Cmd.none
+            )
+
+        StartAnimation ->
+            case model.showMenu of
+                True ->
+                    ( { model
+                        | showMenu = False
+                        , menuBarAnimation = View.MenuBar.startAnimation model
+                        , menuAnimation =
+                            model.menuAnimation
+                                |> Animation.interrupt
+                                    [ Animation.toWith interpolation
+                                        [ Animation.opacity 100
+                                        ]
+                                    ]
+                      }
+                    , Cmd.none
+                    )
+
+                False ->
+                    ( { model
+                        | showMenu = True
+                        , menuBarAnimation = View.MenuBar.startAnimation model
+                        , menuAnimation =
+                            model.menuAnimation
+                                |> Animation.interrupt
+                                    [ Animation.toWith interpolation
+                                        [ Animation.opacity 100
+                                        ]
+                                    ]
+                      }
+                    , Cmd.none
+                    )
+
+        Animate time ->
+            ( { model
+                | styles = List.map (Animation.update time) model.styles
+                , menuBarAnimation = View.MenuBar.update time model.menuBarAnimation
+                , menuAnimation = Animation.update time model.menuAnimation
+              }
+            , Cmd.none
+            )
+
+        OnPageChange _ ->
+            ( { model
+                | showMenu = False
+                , menuBarAnimation = View.MenuBar.init
+              }
+            , Cmd.none
+            )
+
+        OnAirUpdated result ->
+            case result of
+                Ok isOnAir ->
+                    ( { model | isOnAir = isOnAir }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        GotCurrentTime posix ->
+            ( { model | now = Just posix }, Cmd.none )
+
+        SharedMsg sharedMsg ->
+            case sharedMsg of
+                NoOp ->
+                    ( model, Cmd.none )
+
+
+subscriptions : Path -> Model -> Sub Msg
+subscriptions _ model =
+    Sub.batch
+        [ Animation.subscription Animate
+            (model.styles
+                ++ View.MenuBar.animationStates model.menuBarAnimation
+                ++ [ model.menuAnimation ]
+            )
+        , Browser.Events.onResize WindowResized
+
+        --, Time.every (oneSecond * 60) GotCurrentTime
+        ]
+
+
+interpolation =
+    Animation.easing
+        { duration = second * 1
+        , ease = Ease.inOutCubic
+        }
--- src/View.elm
+++ src/View.elm
@@ -1,23 +1,24 @@
 module View exposing (View, map, placeholder)
 
+import Element exposing (Element)
 import Html exposing (Html)
 
 
 type alias View msg =
     { title : String
-    , body : List (Html msg)
+    , body : List (Element msg)
     }
 
 
 map : (msg1 -> msg2) -> View msg1 -> View msg2
 map fn doc =
     { title = doc.title
-    , body = List.map (Html.map fn) doc.body
+    , body = List.map (Element.map fn) doc.body
     }
 
 
 placeholder : String -> View msg
 placeholder moduleName =
     { title = "Placeholder - " ++ moduleName
-    , body = [ Html.text moduleName ]
+    , body = [ Element.text moduleName ]
     }
