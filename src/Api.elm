22d69ec1e1a82136a35e7d9843abe71f4942655e
Dillon Kearns <dillon@dillonkearns.com>
2021-06-25 17:14:24 -0700

    Remove unused module, and comment out API value that's causing errors in the build command for now.

--- src/Api.elm
+++ src/Api.elm
@@ -21,26 +21,26 @@ routes :
     -> (Html Never -> String)
     -> List (ApiRoute.Done ApiRoute.Response)
 routes getStaticRoutes htmlToString =
-    [ ApiRoute.succeed
-        (tipsFeedItems htmlToString
-            |> DataSource.map
-                (\feedItems ->
-                    Rss.generate
-                        { title = "Incremental Elm Tips"
-                        , description = "Incremental Elm Consulting"
-                        , url = Site.canonicalUrl ++ "/tips"
-                        , lastBuildTime = Pages.builtAt
-                        , generator = Just "elm-pages"
-                        , items = feedItems
-                        , siteUrl = Site.canonicalUrl
-                        }
-                )
-            |> DataSource.map (\body -> { body = body })
-        )
-        |> ApiRoute.literal "tips"
-        |> ApiRoute.slash
-        |> ApiRoute.literal "feed.xml"
-        |> ApiRoute.single
+    [--ApiRoute.succeed
+     --    (tipsFeedItems htmlToString
+     --        |> DataSource.map
+     --            (\feedItems ->
+     --                Rss.generate
+     --                    { title = "Incremental Elm Tips"
+     --                    , description = "Incremental Elm Consulting"
+     --                    , url = Site.canonicalUrl ++ "/tips"
+     --                    , lastBuildTime = Pages.builtAt
+     --                    , generator = Just "elm-pages"
+     --                    , items = feedItems
+     --                    , siteUrl = Site.canonicalUrl
+     --                    }
+     --            )
+     --        |> DataSource.map (\body -> { body = body })
+     --    )
+     --    |> ApiRoute.literal "tips"
+     --    |> ApiRoute.slash
+     --    |> ApiRoute.literal "feed.xml"
+     --    |> ApiRoute.single
     ]
 
 
@@ -196,3 +196,139 @@ tipToFeedItem { metadata, body, slug } =
 --        , now = Pages.builtAt
 --        }
 --        metadataToRssItem
+{-
+
+   module Main exposing (main)
+
+   import IcalFeed
+   import MarkdownRenderer
+   import MarkdownToHtmlStringRenderer
+   import Pages
+   import Pages.PagePath as PagePath exposing (PagePath)
+   import Pages.Platform exposing (Page)
+   import Pages.StaticHttp as StaticHttp
+   import Request
+   import Request.Events exposing (LiveStream)
+   import Rss
+   import RssPlugin
+   import Site
+   import TemplateModulesBeta
+   import TemplateType exposing (TemplateType)
+   import Time
+   import UpcomingEvent
+
+
+
+   --main : Pages.Platform.Program Model Msg TemplateType (List (Element Msg))
+
+
+   main =
+       TemplateModulesBeta.mainTemplate
+           { documents =
+               [ { extension = "md"
+                 , metadata = TemplateType.decoder
+                 , body = MarkdownRenderer.view
+                 }
+               ]
+           , site = Site.config
+           }
+           |> Pages.Platform.withFileGenerator
+               (\_ ->
+                   StaticHttp.map
+                       (\events ->
+                           [ Ok
+                               { path = [ "live.ics" ]
+                               , content = IcalFeed.feed events
+                               }
+                           , events
+                               |> List.sortBy
+                                   (\event ->
+                                       Time.posixToMillis event.startsAt
+                                   )
+                               |> List.reverse
+                               |> List.head
+                               |> Maybe.map
+                                   (\upcoming ->
+                                       UpcomingEvent.json upcoming
+                                   )
+                               |> (\json ->
+                                       case json of
+                                           Just value ->
+                                               Ok value
+
+                                           Nothing ->
+                                               Err "No upcoming events found."
+                                  )
+                           ]
+                       )
+                       (Request.staticGraphqlRequest Request.Events.selection)
+               )
+           |> RssPlugin.generate
+               { siteTagline = Site.tagline
+               , siteUrl = Site.canonicalUrl
+               , title = "Incremental Elm Tips"
+               , builtAt = Pages.builtAt
+               , indexPage = Pages.pages.tips
+               , now = Pages.builtAt
+               }
+               metadataToRssItem
+           |> Pages.Platform.toProgram
+
+
+   metadataToRssItem :
+       { path : PagePath Pages.PathKey
+       , frontmatter : TemplateType
+       , body : String
+       }
+       -> Maybe (Result String Rss.Item)
+   metadataToRssItem page =
+       case page.frontmatter of
+           TemplateType.Tip tip ->
+               page.body
+                   |> MarkdownToHtmlStringRenderer.renderMarkdown
+                   |> Result.map
+                       (\markdownHtmlString ->
+                           { title = tip.title
+                           , description = tip.description
+                           , url = PagePath.toString page.path
+                           , categories = []
+                           , author = "Dillon Kearns"
+                           , pubDate = Rss.Date tip.publishedAt
+                           , content = Just markdownHtmlString
+                           , contentEncoded = Just markdownHtmlString
+                           , enclosure = Nothing
+                           }
+                       )
+                   |> Just
+
+           _ ->
+               Nothing
+
+
+   ensureAtPrefix : String -> String
+   ensureAtPrefix twitterUsername =
+       if twitterUsername |> String.startsWith "@" then
+           twitterUsername
+
+       else
+           "@" ++ twitterUsername
+
+
+   fullyQualifiedUrl : String -> String
+   fullyQualifiedUrl url =
+       let
+           urlWithoutLeadingSlash =
+               if url |> String.startsWith "/" then
+                   url |> String.dropLeft 1
+
+               else
+                   url
+       in
+       "https://incrementalelm.com" ++ url
+
+
+   siteName =
+       "Incremental Elm Consulting"
+
+
+-}
