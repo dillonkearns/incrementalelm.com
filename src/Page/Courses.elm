154f93741187f3d8640faedf186fb669dafb7e6d
Dillon Kearns <dillon@dillonkearns.com>
2021-11-08 11:39:07 -0800

    Update courses page.

--- src/Page/Courses.elm
+++ src/Page/Courses.elm
@@ -1,13 +1,20 @@
-module Page.Courses exposing (Model, Msg, Data, page)
+module Page.Courses exposing (Data, Model, Msg, page)
 
+import CourseIcon
+import Css
 import DataSource exposing (DataSource)
 import Head
 import Head.Seo as Seo
+import Html.Styled as Html
+import Html.Styled.Attributes as Attr exposing (css)
+import Link
 import Page exposing (Page, PageWithState, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
+import Route
 import Shared
-import View exposing (View)
+import Tailwind.Utilities as Tw
+import View exposing (Body(..), View)
 
 
 type alias Model =
@@ -17,8 +24,10 @@ type alias Model =
 type alias Msg =
     Never
 
+
 type alias RouteParams =
     {}
+
 
 page : Page RouteParams Data
 page =
@@ -64,4 +73,57 @@ view :
     -> StaticPayload Data RouteParams
     -> View Msg
 view maybeUrl sharedModel static =
-    View.placeholder "Courses"
+    { title = "Placeholder"
+    , body =
+        Tailwind
+            [ Route.Courses__Course___Section_
+                { course = "elm-ts-interop"
+                , section = "intro"
+                }
+                |> Link.htmlLink2
+                    [ css
+                        [ Tw.border_2
+                        , Tw.border_foreground
+                        , Tw.rounded_lg
+                        , Tw.flex
+                        , Tw.flex_col
+                        , Tw.items_center
+                        , Tw.gap_4
+                        , Tw.p_6
+                        ]
+                    ]
+                    [ CourseIcon.elmTsInterop
+                    , Html.div []
+                        [ Html.h2
+                            [ css
+                                [ Tw.font_bold
+                                , Tw.text_xl
+                                ]
+                            ]
+                            [ Html.text "elm-ts-interop"
+                            ]
+                        ]
+                    , Html.p []
+                        [ Html.text "Intro to the basics of using elm-ts-interop to generate TypeScript bindings for type-safe ports and flags."
+                        ]
+                    , Html.a
+                        [ css
+                            [ Css.hover
+                                [ Tw.bg_foregroundStrong
+                                , Tw.underline
+                                ]
+                            , Tw.px_4
+                            , Tw.py_2
+                            , Tw.bg_foreground
+                            , Tw.text_background
+                            , Tw.rounded_lg
+                            , Tw.text_center
+                            , Tw.cursor_pointer
+                            ]
+                        , Attr.href "/courses/elm-ts-interop/intro"
+                        ]
+                        [ Html.text "Watch Now"
+                        ]
+                    ]
+            ]
+    }
